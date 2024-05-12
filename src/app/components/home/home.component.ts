46fbb2701589b8b3e1ef8699c2ef71208ca98280
Shohail <shohail@corzent.com>
2024-05-12 23:12:57 +0530

    ui improvement

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,6 +1,9 @@
-import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, RouterModule } from '@angular/router';
 import { SidebarComponent } from '../layout/sidebar/sidebar.component';
+import { EMPTY, switchMap } from 'rxjs';
+import { UserService } from '@app/services/user.service';
+import { User } from '@app/models/user.model';
 
 @Component({
   selector: 'app-home',
@@ -9,6 +12,32 @@ import { SidebarComponent } from '../layout/sidebar/sidebar.component';
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
   sidebarExpanded = false;
+  currentUser!: User;
+
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private userService: UserService
+  ) {}
+
+  ngOnInit(): void {
+    this.subscribeToUserById();
+  }
+
+  private subscribeToUserById(): void {
+    this.activatedRoute.paramMap
+      .pipe(
+        switchMap((params) => {
+          const userIdFromRoute = Number(params.get('userId'));
+          if (userIdFromRoute) {
+            return this.userService.getUserById(userIdFromRoute);
+          }
+          return EMPTY;
+        })
+      )
+      .subscribe((user: User) => {
+        this.currentUser = user;
+      });
+  }
 }
